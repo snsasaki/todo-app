cfe7c4e2a6b721f727a078ddc6e32c3a68d6928a
snsasaki <sn.sasaki@core-tech.jp>
2026-04-17 13:26:20 +0900

    :sparkles: index.php(雛形)を追加

---
+++ src/index.php
@@ -0,0 +1,22 @@
+<?php
+// 初期化（空配列）
+// $todos = [];
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todoアプリ</title>
+</head>
+<body>
+    <h1>Todoアプリ</h1>
+
+    <form method="POST" action="">
+        <input type="text" name="title" placeholder="Todoを入力">
+        <button type="submit">追加</button>
+    </form>
+
+</body>
+</html>
